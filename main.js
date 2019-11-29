f14c75136a3ff251e8a93f2cbc8de2409db6b2d4
Alex Reid <42113044+Alexreid95@users.noreply.github.com>
2019-11-29 16:31:56 +0000

    Dot Navigation operational
    Dot Navigation should now move between slides and be opaque on active dot

--- main.js
+++ main.js
@@ -2,15 +2,27 @@
 let slideImg = document.querySelectorAll(".slide");
 let arrowLeft = document.querySelector('#arrow-left');
 let arrowRight = document.querySelector('#arrow-right');
-let slideIdx = 0;
+var slideIdx = 0;
 var captionTxt = document.querySelector(".caption-holder .caption-txt");
 var dotCont = document.getElementById("dot-cont");
 var dots = [];
+
+//Returns the Dot Navigation
+function dotNav() {
+  for (let i = 0; i < slideImg.length; i++) {
+    var dot = document.createElement("span");
+    dot.classList.add("dots");
+    dot.setAttribute("onClick", "moveSlide("+i+")")
+    dotCont.append(dot);
+    dots.push(dot);
+  }
+}
 
 //Hides all slide Images
 function reset() {
   for (let i = 0; i < slideImg.length; i++) {
     slideImg[i].style.display = "none";
+    dots[slideIdx].classList.remove("active");
   }
 }
 
@@ -18,23 +30,27 @@ function reset() {
 function displaySlide() {
   slideImg[slideIdx].style.display = "block";
   captionTxt.innerText = slideImg[slideIdx].querySelector(".caption-txt").innerText;
-}
-
-//Returns the Dot Navigation
-function dotNav() {
-  for (let i = 0; i < slideImg.length; i++) {
-    var dot = document.createElement("span");
-    dot.classList.add("dots");
-    dotCont.append(dot);
-    dots.push(dot);
-  }
+  dots[slideIdx].classList.add("active");
 }
 
 //Init first slide
 function startSlides() {
+  dotNav();
   reset();
   displaySlide();
-  dotNav();
+}
+
+//For dot navigation, to move to correct slide
+function moveSlide(slideIdx) {
+  reset();
+  slideImg[slideIdx].style.display = "block";
+  captionTxt.innerText = slideImg[slideIdx].querySelector(".caption-txt").innerText;
+  dots[slideIdx].classList.add("active");
+  for (let i=0; i<slideImg.length; i++) {
+    if (i!==slideIdx) {
+      dots[i].classList.remove("active");
+    }
+  }
 }
 
 //Move to previous slide when the  left arrow is clicked with mouse
