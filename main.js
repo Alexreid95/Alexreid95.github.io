693a985cf8b7998bc23ca136b86d32e9b8fcba1f
Alex Reid <42113044+Alexreid95@users.noreply.github.com>
2019-12-12 16:35:08 +0000

    Mega Update main.js, Carousel and text
    - Re-organised here and there
    - Added carousel and slide variables 
    - Added to reset(), currentTxt value and textHolder
    - Added to displaySlide(), carousel movement, slideTxt and slideTxt animation
    - Reorganised and Added to toPrev() & toNext(), textholderAnimation

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 // Variables
-// Slides and arrows
+// Slide Images and arrows
 let slideImg = document.querySelectorAll(".slide");
 let arrowLeft = document.querySelector('#arrow-left');
 let arrowRight = document.querySelector('#arrow-right');
 var slideIdx = 0;
-//Slide Animation
-
 // Caption text
 var captionTxt = document.querySelector(".caption-holder .caption-txt");
-//Caption Animation
 var captionAnimation = "slideCaptionUp";
+// Carousel and Slide
+var slideTxt = document.querySelector(".text-holder .slide-txt");
+var textHolder = document.querySelectorAll(".carousel .text-holder");
+var textHolderCurrentAnimation = "slideCurrentLeft";
+var textHolderNextAnimation = "slideNextLeft";
+var currentTxt, nextTxt;
 // Dot navigation
 var dotCont = document.getElementById("dot-cont");
 var dots = [];
@@ -30,25 +33,39 @@ function dotNav() {
   }
 }
 
-//Hides all slide Images
+//Preperation for next slide
 function reset() {
+  currentTxt = textHolder[slideIdx];
   for (let i = 0; i < slideImg.length; i++) {
     slideImg[i].style.display = "none";
     dots[i].classList.remove("active");
+    textHolder[i].className = "text-holder";
+    textHolder[i].style.opacity = 0;
   }
 }
 
-//Returns the right slide and caption
+//Displays the right information for slide
 function displaySlide() {
   if (auto !== null) {
     pauseSlide();
     autoLoopSlides();
   }
+  //Carousel movement
+  nextTxt = textHolder[slideIdx];
+  currentTxt.classList.add(textHolderCurrentAnimation);
+  nextTxt.classList.add(textHolderNextAnimation);
+  //Page Image
   slideImg[slideIdx].style.display = "block";
+  //Caption text
   captionTxt.style.display = "none";
   captionTxt.className = "caption-txt " + captionAnimation;
   captionTxt.innerText = slideImg[slideIdx].querySelector(".caption-txt").innerText;
   captionTxt.style.display = "block";
+  //Slide text and animation
+  slideTxt.style.animation = "none";
+  slideTxt.innerText = slideImg[slideIdx].querySelector(".slide-txt").innerText;
+  slideTxt.style.animation = "";
+  //Dot selection
   dots[slideIdx].classList.add("active");
 }
 
@@ -83,32 +100,28 @@ function pauseSlide() {
   auto === null;
 }
 
-//Move to previous slide when the  left arrow is clicked with mouse
+//Move to previous slide
 var toPrev = function() {
   reset();
   captionAnimation = "slideCaptionRight";
-  if (slideIdx === 0) {
-    slideIdx = slideImg.length - 1;
-    displaySlide();
-  } else {
-    slideIdx--;
-    displaySlide();
-  }
+  textHolderCurrentAnimation = "slideCurrentRight";
+  textHolderNextAnimation = "slideNextRight";
+  (slideIdx === 0) ? slideIdx = slideImg.length - 1:slideIdx--;
+  displaySlide();
 }
+//Move to previous when left arrow is clicked with mouse
 arrowLeft.addEventListener("click", toPrev);
 
-//Move to next slide when the right arrow is clicked with mouse
+//Move to next slide
 var toNext = function() {
   reset();
   captionAnimation = "slideCaptionLeft";
-  if (slideIdx === slideImg.length - 1) {
-    slideIdx = 0;
-    displaySlide();
-  } else {
-    slideIdx++;
-    displaySlide();
-  }
+  textHolderCurrentAnimation = "slideCurrentLeft";
+  textHolderNextAnimation = "slideNextLeft";
+  (slideIdx === slideImg.length - 1) ? slideIdx = 0:slideIdx++;
+  displaySlide();
 }
+//Move to Next when right arrow is clicked with mouse
 arrowRight.addEventListener("click", toNext);
 
 //Slide change when keyboard arrow is pressed
